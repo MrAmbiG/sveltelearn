9571282c6c409735d2a58cca866d82082ab6e161
Gajendra D Ambi <gajuambi@Gajendras-MacBook-Pro.local>
2023-06-01 02:08:37 +0530

    26 going

--- sk-loading-data/src/routes/products/+page.svelte
+++ sk-loading-data/src/routes/products/+page.svelte
@@ -1,2 +1,14 @@
-<h1>list of products</h1>
-<p></p>
+
+<script>
+    export let data;
+    const products = data.products;
+</script>
+
+<h1>{data.title}</h1>
+{#each products as product}
+<div>
+    <h2>{product.title}</h2>
+    <p>{product.description}</p>
+    <hr/>
+</div>
+{/each}
