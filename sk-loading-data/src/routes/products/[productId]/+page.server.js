f3dca11eb588841057a95782cd0d8c54bed7616e
Gajendra D Ambi <gajuambi@Gajendras-MacBook-Pro.local>
2023-06-02 01:41:38 +0530

    to do 30

---
+++ sk-loading-data/src/routes/products/[productId]/+page.server.js
@@ -0,0 +1,13 @@
+
+export const load = async (serverLoadEvent) => {
+    const { fetch, params, url, route } = serverLoadEvent
+    console.log({params, url, route: route.id})
+    const { productId} = params
+    const title = "product details";
+    const response = await fetch(`http://localhost:4000/products/${productId}`);
+    const product = await response.json();
+    return {
+        title,
+        product,
+    }
+}
