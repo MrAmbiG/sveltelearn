768eda6910975b2d15116674186074bbfd5ee3e9
Gajendra D Ambi <gajuambi@Gajendras-MacBook-Pro.local>
2023-06-08 01:07:17 +0530

    2 vids

--- sk-loading-data/src/routes/+page.svelte
+++ sk-loading-data/src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+
+<h1 class="tall">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href='/products'>list of products</a>
+<a href='/products' data-sveltekit-preload-code="viewport">Products</a>
+
+<style>
+    .tall {
+        height: 100px
+    }
+</style>
