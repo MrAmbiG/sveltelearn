382c2b0f10a339cdf10ed514cc8261ca565edb4a
Gajendra D Ambi <gajuambi@Gajendras-MacBook-Pro.local>
2023-06-29 05:44:45 +0530

    adding css var

--- sveltekit-walkthrough/src/routes/+page.svelte
+++ sveltekit-walkthrough/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit walkthrough</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import Hero from "./Hero.svelte";
+</script>
+<Hero />
+Home
---
+++ sveltekit-walkthrough/src/routes/Hero.svelte
@@ -0,0 +1,30 @@
+
+<div class="hero">
+    <div class="container">
+
+
+    <h2>Business Name or Tagline</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, culpa. Animi velit illum repudiandae voluptatibus placeat recusandae tempora, provident adipisci aut totam sed voluptates, nostrum itaque voluptate quidem deserunt at!
+    </p>
+    </div>
+</div>
+
+<style>
+    .hero {
+        padding: 5em 0;
+        background-color: cornflowerblue;
+        color: white;
+    }
+
+    h2 {
+        margin: 0;
+        font-size: 3.5em;
+        font-weight: 500;
+    }
+    p {
+        color: #ddd;
+        font-size: 1.1em;
+        line-height: 1.4em;
+    }
+</style>
